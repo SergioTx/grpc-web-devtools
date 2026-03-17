9739ade51b1cba55f423b00018ac0250142f6c39
Aidan Jensen <aidan.jensen@robust.ai>
2026-03-17 13:36:23 -0700

    Add support for custom toJson function
    Signed-off-by: Aidan Jensen <aidan.jensen@robust.ai>

--- public/connect-web-interceptor.js
+++ public/connect-web-interceptor.js
@@ -1,8 +1,18 @@
 // This interceptor will be passed every request and response. We will take that request and response
 // and post a message to the window. This will allow us to access this message in the content script. This
 // is all to make the manifest v3 happy.
-const interceptor =
-  (next) =>
+
+/**
+ * Creates an interceptor with optional custom toJson function
+ * @param {Function} [customToJson] - Optional custom function to serialize messages to JSON.
+ *                                     Should accept a message object and return a JSON-serializable object.
+ * @returns {Function} The interceptor function
+ */
+const createInterceptor = (customToJson) => {
+  // Default to using the message's toJson method if no custom function is provided
+  const toJson = customToJson || ((message) => message.toJson());
+
+  return (next) =>
     async (req) => {
       return await next(req).then((resp) => {
         if (!resp.stream) {
@@ -10,8 +20,8 @@ const interceptor =
             type: "__GRPCWEB_DEVTOOLS__",
             methodType: "unary",
             method: req.method.name,
-            request: req.message.toJson(),
-            response: resp.message.toJson(),
+            request: toJson(req.message),
+            response: toJson(resp.message),
           }, "*")
           return resp;
         } else {
@@ -25,8 +35,8 @@ const interceptor =
                   type: "__GRPCWEB_DEVTOOLS__",
                   methodType: "server_streaming",
                   method: req.method.name,
-                  request: req.message.toJson(),
-                  response: streamResp.value.toJson(),
+                  request: toJson(req.message),
+                  response: toJson(streamResp.value),
                 }, "*");
               }
               return streamResp;
@@ -38,7 +48,7 @@ const interceptor =
           type: "__GRPCWEB_DEVTOOLS__",
           methodType: req.stream ? "server_streaming" : "unary",
           method: req.method.name,
-          request: req.message.toJson(),
+          request: toJson(req.message),
           response: undefined,
           error: {
             message: e.message,
@@ -48,8 +58,13 @@ const interceptor =
         throw e;
       });
     };
+};
 
-window.__CONNECT_WEB_DEVTOOLS__ = interceptor;
+// Default interceptor for backward compatibility
+window.__CONNECT_WEB_DEVTOOLS__ = createInterceptor();
+
+// Export the factory function for custom usage
+window.__CREATE_CONNECT_WEB_DEVTOOLS__ = createInterceptor;
 
 // Since we are loading inject.js as a script, the order at which it is loaded is not guaranteed.
 // So we will publish a custom event that can be used, to be used to assign the interceptor.
